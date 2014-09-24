16058bfce59b385e39ac92a3a4d7e9897efb644b
Xuan Luong <luonghoaixuan@gmail.com>
2014-09-24 03:42:35 +0200

    Define the function that checks if the game is won

--- js/board.js
+++ js/board.js
@@ -1,9 +1,9 @@
 var Board = function() {
 	this.cells = [
-		[1,2,3,4],
-		[5,6,7,8],
-		[9,10,11,12],
-		[13,14,15,0]
+		[0,1,2,3],
+		[4,5,6,7],
+		[8,9,10,11],
+		[12,13,14,15]
 	];
 	this.row = 3; // current row of 0
 	this.col = 3; // current col of 0
@@ -63,4 +63,22 @@ Board.prototype.slideDown = function() {
 		console.log("slide down has no effect.");
 	}
 	return this;
+};
+
+Board.prototype.won = function() {
+	var index = [0,1,2,3],
+		row, col;
+	if (this.cells[3][3] === 0) {
+		for (row in index) {
+			for (col in index) {
+				if (row !== 3 && col !== 3) {
+					if (this.cells[row][col] != row * 4 + col) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	return false;
 };
