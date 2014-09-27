2d381038022e341f7f7cd625fc234c1f99099693
Xuan Luong <luonghoaixuan@gmail.com>
2014-09-28 01:59:17 +0200

    animation: make the empty tile move following keydown event. Bug exists when click New Game

--- src/js/directives.js
+++ src/js/directives.js
@@ -5,37 +5,68 @@ var directionMap = {
 	right: [0,1]
 };
 
-var ngBoardAnimate = function($animate) {
-
-	function move(row, col, dir) {
-		var dst = [row,col],
-			src = [row - dir[0], col - dir[1]];
-		if (dst[0] !== src[0] || dst[1] !== src[1]) {
-
-		}
-		console.log(src, dst);
-	}
+var ngBoardAnimate = function($document) {
 
 	return function(scope, element, attrs) {
-		element.on("keydown", function(event) {
+		var size = 110,
+			margin = 12,
+			gap = size + margin,
+			tile = element.find("#zero-tile"),
+			lastRow = scope.board.row,
+			lastCol = scope.board.col,
+			y = lastRow * (size + margin), 
+			x = lastCol * (size + margin);
+
+		tile.css({
+			"margin-top": y + "px",
+			"margin-left": x + "px"
+		});
+
+		function move(row, col, dir) {
+			var dst = [row,col],
+				src = [row - dir[0], col - dir[1]];
+			lastRow = scope.board.row;
+			lastCol = scope.board.col;
+			console.log(src, dst);
+			if (dst[0] !== src[0] || dst[1] !== src[1]) {
+				y += dir[0] * gap;
+				x += dir[1] * gap;
+				tile.css({
+					"margin-top": y + "px",
+					"margin-left": x + "px"
+				});
+			}
+			
+		}
+
+		$document.on("keydown", function(event) {
 			var modifiers = event.altKey || event.ctrlKey || event.metaKey || event.shiftKey;
         	if (!modifiers) {
+        		console.log(lastRow, lastCol);
 				switch (event.which) {
 	        		case 38: //up
 	        			event.preventDefault();
-	        			move(scope.board.row, scope.board.col, directionMap.up);
+	        			if (lastRow > 0) {
+	        				move(scope.board.row, scope.board.col, directionMap.up);
+	        			}
 	        			break;
 	        		case 40: //down
 	        			event.preventDefault();
-	        			move(scope.board.row, scope.board.col, directionMap.down);
+	        			if (lastRow < 3) {
+	        				move(scope.board.row, scope.board.col, directionMap.down);
+	        			}
 	        			break;
 	        		case 37: //left
 	        			event.preventDefault();
-	        			move(scope.board.row, scope.board.col, directionMap.left);
+	        			if (lastCol > 0) {
+	        				move(scope.board.row, scope.board.col, directionMap.left);
+	        			}
 	        			break;
 	        		case 39: //right
 	        			event.preventDefault();
-	        			move(scope.board.row, scope.board.col, directionMap.right);
+	        			if (lastCol < 3) {
+	        				move(scope.board.row, scope.board.col, directionMap.right);
+	        			}
 	        			break;
 	        		default: break;
 	        	}
