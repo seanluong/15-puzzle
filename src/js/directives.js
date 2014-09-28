58684b356b410be12d64681130d874cf11a77bff
Xuan Luong <luonghoaixuan@gmail.com>
2014-09-28 12:20:34 +0200

    animation: add animation, but still have glitches at the beginning

--- src/js/directives.js
+++ src/js/directives.js
@@ -4,25 +4,42 @@ var ngZeroTile = function() {
 		var size = 110,
 			margin = 12,
 			gap = size + margin,
-			lastRow = scope.board.row, 
-			lastCol = scope.board.col, 
-			y = lastRow * (size + margin), 
-			x = lastCol * (size + margin);
+			y = scope.board.row * (size + margin), 
+			x = scope.board.col * (size + margin),
+			oldY = y, oldX = x;
+
 		element.css({
 			"margin-top": y + "px",
 			"margin-left": x + "px"
 		});
 
+		function move() {
+			window.requestAnimationFrame(move);
+			repaint();
+		}
+
+		function repaint() {
+			if (oldY < y) {
+				oldY++;
+			} else if (oldY > y) {
+				oldY--;
+			}
+			if (oldX < x) {
+				oldX++;
+			} else if (oldX > x) {
+				oldX--;
+			}
+			element.css({
+				"margin-top": oldY + "px",
+				"margin-left": oldX + "px"
+			});
+		}
+
 		scope.$on("update", function(event) {
 			event.stopPropagation();
-			lastRow = scope.board.row;
-			lastCol = scope.board.col;
-			y = lastRow * (size + margin); 
-			x = lastCol * (size + margin);
-			element.css({
-				"margin-top": y + "px",
-				"margin-left": x + "px"
-			});
+			y = scope.board.row * (size + margin); 
+			x = scope.board.col * (size + margin);
+			move();
 		});
 	};
 };
