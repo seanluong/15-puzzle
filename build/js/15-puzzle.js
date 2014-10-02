f19724cc6ae567e56f058457baf8141e4ba93013
Xuan Luong <luonghoaixuan@gmail.com>
2014-10-02 22:18:38 +0200

    restructure: add wasd control

--- build/js/15-puzzle.js
+++ build/js/15-puzzle.js
@@ -420,7 +420,11 @@ var keyboardMapService = [	function() {
 		38: "up",
 		40: "down",
 		37: "left",
-		39: "right"
+		39: "right",
+		87: "down",// w
+		65: "right",// a
+		83: "up",// s
+		68: "left"// d
 	};
 	return function(whichKey) {
 		return key2dir[whichKey];
