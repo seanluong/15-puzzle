7467f1dabcec855b6cc6fe4e108b28a491a782f5
Xuan Luong <luonghoaixuan@gmail.com>
2014-10-02 03:25:37 +0200

    Test (5)

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '776055202450751',
+      xfbml      : true,
+      version    : 'v2.1'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 var myApp = angular.module("myApp", ["angular-gestures","ui.bootstrap","djds4rce.angular-socialshare"]);
 myApp.controller("bodyController", bodyController);
 myApp.controller("headerController", headerController);
