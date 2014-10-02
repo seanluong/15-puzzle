374f7bdd982d3d35ed34b19df92e80c3f615880b
Xuan Luong <luonghoaixuan@gmail.com>
2014-10-02 20:25:39 +0200

    restructure: add injection annotation for controllers

--- build/js/15-puzzle.js
+++ build/js/15-puzzle.js
@@ -410,9 +410,9 @@ var ngTile = function() {
 
 };
 var myApp = angular.module("myApp", ["angular-gestures","ui.bootstrap","djds4rce.angular-socialshare"]);
-myApp.controller("bodyController", bodyController);
-myApp.controller("headerController", headerController);
-myApp.controller("mainController", mainController);
+myApp.controller("bodyController", ["$scope", "$modal", "$document", bodyController]);
+myApp.controller("headerController", ["$scope", "$interval", "$timeout", "$modal", headerController]);
+myApp.controller("mainController", ["$scope", "$document", "$timeout", mainController]);
 myApp.filter("duration", durationFilter);
 myApp.directive('ngTile', ["$interval", ngTile]);
 myApp.run(function() {
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 var myApp = angular.module("myApp", ["angular-gestures","ui.bootstrap","djds4rce.angular-socialshare"]);
-myApp.controller("bodyController", bodyController);
-myApp.controller("headerController", headerController);
-myApp.controller("mainController", mainController);
+myApp.controller("bodyController", ["$scope", "$modal", "$document", bodyController]);
+myApp.controller("headerController", ["$scope", "$interval", "$timeout", "$modal", headerController]);
+myApp.controller("mainController", ["$scope", "$document", "$timeout", mainController]);
 myApp.filter("duration", durationFilter);
 myApp.directive('ngTile', ["$interval", ngTile]);
 myApp.run(function() {
