7ef1e62f52ff1c2857d558788a4acf54465c149b
Xuan Luong <luonghoaixuan@gmail.com>
2014-10-02 03:09:29 +0200

    Add fb share script

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,18 @@ $("#board-container").on('touchmove', function(e) {
 myApp.run(function($FB){
   $FB.init('776055202450751');
 });
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '776055202450751',
+  xfbml      : true,
+  version    : 'v2.1'
+});
+};
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
